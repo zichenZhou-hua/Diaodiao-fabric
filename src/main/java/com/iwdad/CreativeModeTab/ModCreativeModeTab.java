d52568dfca03895326da3eadde14ca29e5e15ddb
zichenZhou-hua <196445106+zichenZhou-hua@users.noreply.github.com>
2026-07-30 09:20:09 +0800

    合并更改

--- src/main/java/com/iwdad/CreativeModeTab/ModCreativeModeTab.java
+++ src/main/java/com/iwdad/CreativeModeTab/ModCreativeModeTab.java
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
-package com.iwdad.CreativeModeTab;
+package com.iwdad.ModCreativeModeTab;
 
 public class ModCreativeModeTab {
 
 }
-=======
-package com.iwdad.CreativeModeTab;
-
-public class ModCreativeModeTab {
-
-}
->>>>>>> 90e2e8f3b5d96c002e613eddb00c9b14f5df95fa
